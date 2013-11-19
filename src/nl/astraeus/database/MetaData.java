a73b755cee9a27423114a196b03f592f78b77408
rnentjes <rien@nentjes.com>
2013-11-20 00:06:27 +0100

    Database mapping, added Boolean

--- src/nl/astraeus/database/MetaData.java
+++ src/nl/astraeus/database/MetaData.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 import java.sql.*;
 import java.util.*;
 import java.util.Date;
@@ -430,10 +431,14 @@ public class MetaData<T> {
             statement.setShort(index, (Short)param);
         } else if (param.getClass().equals(Double.class) || param.getClass().equals(double.class)) {
             statement.setDouble(index, (Double)param);
+        } else if (param.getClass().equals(BigDecimal.class)) {
+            statement.setBigDecimal(index, (BigDecimal) param);
+        } else if (param.getClass().equals(Boolean.class) || param.getClass().equals(boolean.class)) {
+            statement.setBoolean(index, (Boolean) param);
         } else if (param.getClass().equals(Date.class)) {
-            java.sql.Date date = new java.sql.Date(((Date)param).getTime());
+            Timestamp date = new java.sql.Timestamp(((Date)param).getTime());
 
-            statement.setDate(index, date);
+            statement.setTimestamp(index, date);
         } else {
             throw new IllegalStateException("Type "+param.getClass()+" not supported in where queries yet!");
         }
